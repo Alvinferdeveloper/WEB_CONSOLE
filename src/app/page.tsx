8dfdada59ee389ad0cd50b96abf3e33320c5177a
Albin <alvinfer67@gmail.com>
2025-04-23 12:41:14 -0600

    add: scanline effect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ export default function App() {
   return (
     <SessionProvider>
       <div className=' h-screen overflow-x-auto bg-zinc-950'>
+        <div className="absolute top-0 left-0 w-full h-[4px] bg-terminal-green/10 animate-scanline pointer-events-none z-10"></div>
         <Cli />
       </div>
     </SessionProvider>
